8bf956a3a7e143799d2656bf2c17b697f6ce3d18
Sam Roberts <sam.roberts.1983@gmail.com>
2015-01-12 13:59:34 +1100

    remove all code except CompactJob and dependencies

--- src/main/scala/com/stripe/herringbone/load/ParquetLoader.scala
+++
@@ -1,9 +0,0 @@
-package com.stripe.herringbone.load
-
-trait ParquetLoader {
-  def checkTableExists(table: String, db: String): Boolean
-  def updateTable(table: String, db: String): Unit
-  def createTable(path: String, table: String, db: String): Unit
-  def closeConnection(): Unit
-}
-
